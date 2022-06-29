c8893ef75ded124893f34eee3b9df38f8ba9ef63
idooldima <idooldima@gmail.com>
2022-06-29 22:40:03 +0300

    add hover card

--- src/pages/home/listItems/listItemCard/listItemCard.styles.ts
+++ src/pages/home/listItems/listItemCard/listItemCard.styles.ts
@@ -1,20 +1,23 @@
 export const style = {
-  card: {
-    minWidth: 420,
-  },
-  taskBtn: {
-    minWidth: 0,
-    color: 'black',
-    border: 'none',
-  },
-  container: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    margin: 'unset',
-    padding: 'unset',
-  },
-  itemId: {
-    textAlign: 'end',
-    fontSize: 12,
-  },
+    card: {
+        minWidth: 420,
+        "&:hover": {
+            boxShadow: 8,
+        },
+    },
+    taskBtn: {
+        minWidth: 0,
+        color: 'black',
+        border: 'none',
+    },
+    container: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        margin: 'unset',
+        padding: 'unset',
+    },
+    itemId: {
+        textAlign: 'end',
+        fontSize: 12,
+    },
 };
